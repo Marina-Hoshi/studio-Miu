72ef5e01ef56267f6f30c029ccc8dfb5969941a0
{Marina <mari_ic5paniy@icloud.com>
2024-03-28 01:02:49 +0900

    main contents3 jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,6 +50,52 @@ $(() => {
     });
   };
 
+
+
+  $('#circle-container').append('<div id="inner-circle"></div>');
+  $('#inner-circle').animate({
+    'top': '0'
+  }, 1000, function() {
+    setTimeout(function() {
+      $('.c-slide__left').append('<div id="yellow-background"></div>');
+      $('#yellow-background').animate({
+        'top': '0'
+      }, 1000, function() {
+        $('#circle-container, #yellow-background').animate({
+          'top': '100%'
+        }, 1000, function() {
+          $('#inner-circle').css('top', '-100%');
+          $('#yellow-background').remove();
+          $('#circle-container').animate({
+            'top': '50%'
+          }, 1000);
+        });
+      });
+    }, 2000);
+  });
+
+  setInterval(function() {
+    $('#inner-circle').animate({
+      'top': '0'
+    }, 1000, function() {
+      setTimeout(function() {
+        $('#yellow-background').animate({
+          'top': '0'
+        }, 1000, function() {
+          $('#circle-container, #yellow-background').animate({
+            'top': '100%'
+          }, 1000, function() {
+            $('#inner-circle').css('top', '-100%');
+            $('#yellow-background').remove();
+            $('#circle-container').animate({
+              'top': '50%'
+            }, 1000);
+          });
+        });
+      }, 2000);
+    });
+  }, 7000);
+
   $(window).scroll(() => {
     const scrollTop = $(window).scrollTop();
     const windowHeight = $(window).height();
@@ -59,16 +105,5 @@ $(() => {
     checkScrollSlideFadeLeft(scrollBottom);
     checkScrollSlideFadeRight(scrollBottom);
   });
-
-  $('.c-left__img:nth-child(n+2)').hide();
-setInterval(function() {
-  $(".c-left__img:first-child").animate({top: '100%'}, 1000, function() {
-    $(this).hide();
-    $(this).css('top', '0');
-  });
-  $(".c-left__img:nth-child(4)").css('top', '-100%').show().animate({top: '0'}, 1000);
-  $(".c-left__img:nth-child(2)").fadeIn(2000);
-  $(".c-left__img:first-child").appendTo(".c-slide__left");
-}, 4000);
 });
 

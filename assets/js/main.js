ed7b4f2d9b6451f28e66fcb973dbb0c1917d3825
{Marina <mari_ic5paniy@icloud.com>
2024-03-31 00:53:55 +0900

    main contents3

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,62 +50,6 @@ $(() => {
     });
   };
 
-
-
-  const $leftInnerCircle = $(".c-slide__left .c-inner-circle");
-  const $rightInnerCircle = $(".c-slide__right .c-inner-circle");
-
-  function animateLeft() {
-    $leftInnerCircle.animate({
-      top: "100%" // 上から下にスライドイン
-    }, {
-      duration: 1500,
-      complete: function() {
-        setTimeout(function() {
-          $leftInnerCircle.animate({
-            top: "-100%" // 下から上にスライドアウト
-          }, {
-            duration: 1500,
-            complete: function() {
-              setTimeout(function() {
-                animateLeft(); // 再帰的に自身を呼び出す
-              }, 5000); // 5秒待機
-            }
-          });
-        }, 5000); // 5秒待機
-      }
-    });
-  }
-
-  function animateRight() {
-    $rightInnerCircle.animate({
-      top: "-100%" // 下から上にスライドイン
-    }, {
-      duration: 1500,
-      complete: function() {
-        setTimeout(function() {
-          $rightInnerCircle.animate({
-            top: "100%" // 上から下にスライドアウト
-          }, {
-            duration: 1500,
-            complete: function() {
-              setTimeout(function() {
-                animateRight(); // 再帰的に自身を呼び出す
-              }, 5000); // 5秒待機
-            }
-          });
-        }, 5000); // 5秒待機
-      }
-    });
-  }
-
-  // 初回呼び出し
-  setTimeout(function() {
-    animateLeft(); // 左側のアニメーションを開始
-    animateRight(); // 右側のアニメーションを開始
-  }, 5000); // 最初のアニメーションを待つ時間
-
-
   $(window).scroll(() => {
     const scrollTop = $(window).scrollTop();
     const windowHeight = $(window).height();

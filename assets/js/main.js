b2372b9b4a4f9219d9e587f48263b31299150d14
{Marina <mari_ic5paniy@icloud.com>
2024-05-02 01:34:08 +0900

    jquery css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(() => {
+$(document).ready(function() {
   // ページ読み込み時にローディング画面を表示
   $("#loading-screen").fadeIn(1000);
 
@@ -10,155 +10,66 @@ $(document).ready(() => {
 
   // 一定時間後に要素の表示を開始
   setTimeout(() => {
-    $(".c-slideImage, .c-scroll-line, .p-subNav__menu--pc").addClass("_isActiveFadeIn");
-  }, 500);
-
+    $(".c-slideImage, .c-scroll-line, .p-subNav__menu--pc, .p-nav__menu--pc").addClass("_isActiveFadeIn");
+  }, 600);
   setTimeout(() => {
-    $("h1").addClass("_isActiveFadeIn");
+    $("h1, .c-top__subTitle").addClass("_isActiveFadeIn");
   }, 1200);
 
-  setTimeout(() => {
-    $(".c-top__subTitle").addClass("_isActiveFadeIn");
-  }, 1200);
-});
+  // スクロールイベントを監視してフェードインさせる
+  $(window).scroll(() => {
+    $(".p-about__imgTop, .p-about__imgBottom, .c-top__subTitle").each((index, element) => {
+      const position = $(element).offset().top;
+      const windowHeight = $(window).height();
+      const scroll = $(window).scrollTop();
 
+      if (scroll > position - windowHeight + 200) {
+        $(element).addClass("_isActiveFadeIn");
+      }
+    });
+  });
 
+  // 擬似要素のhover
+  $('.c-nav__menu').hover(
+    function() {
+      $(this).addClass('hovered');
+    },
+    function() {
+      $(this).removeClass('hovered');
+    }
+  );
 
-
-// $(document).ready(() => {
-//   $("body").fadeIn(1000);
-
-//   setTimeout(() => {
-//     $(".c-slideImage, .c-scroll-line, .p-subNav__menu--pc").addClass("_isActiveFadeIn");
-//   }, 500);
-
-//   setTimeout(() => {
-//     $("h1").addClass("_isActiveFadeIn");
-//   }, 1200);
-
-//   setTimeout(() => {
-//     $(".c-top__subTitle").addClass("_isActiveFadeIn");
-//   }, 1200);
-// });
-
-
-// $(() => {
-//   const fadeInPage = () => {
-//     $('body').fadeIn(1000);
-//   };
-
-//   fadeInPage();
-
-//   const checkScrollFadeIn = (scrollBottom) => {
-//     const scrollFadeIn = $("._scrollFadeIn");
-
-//     scrollFadeIn.each((index, element) => {
-//       const $element = $(element);
-//       const elementTop = $element.offset().top;
-
-//       if (elementTop < scrollBottom) {
-//         $element.addClass("_isActiveFadeIn");
-//       } else {
-//         $element.removeClass("_isActiveFadeIn");
-//       }
-//     });
-//   };
-
-
-//   $(window).scroll(() => {
-//     const scrollTop = $(window).scrollTop();
-//     const windowHeight = $(window).height();
-//     const scrollBottom = scrollTop + windowHeight;
-
-//     checkScrollFadeIn(scrollBottom);
-//     checkScrollSlideFadeLeft(scrollBottom);
-//     checkScrollSlideFadeRight(scrollBottom);
-//   });
-// });
-
-
-
-// $(document).ready(function() {
-//     const currentIndexLeft = 0;
-//     const currentIndexRight = 0;
-//     const imagesLeft = $('.p-header__left img');
-//     const imagesRight = $('.p-header__right img');
-//     const totalImagesLeft = imagesLeft.length;
-//     const totalImagesRight = imagesRight.length;
-
-//     function showNextImageLeft() {
-//       const nextIndex = (currentIndexLeft + 1) % totalImagesLeft;
-//       imagesLeft.eq(currentIndexLeft).animate({opacity: 0}, 500);
-//       imagesLeft.eq(nextIndex).animate({opacity: 1}, 500);
-//       currentIndexLeft = nextIndex;
-//     }
-
-//     function showNextImageRight() {
-//       const nextIndex = (currentIndexRight + 1) % totalImagesRight;
-//       imagesRight.eq(currentIndexRight).animate({opacity: 0}, 500);
-//       imagesRight.eq(nextIndex).animate({opacity: 1}, 500);
-//       currentIndexRight = nextIndex;
-//     }
-
-//     setInterval(showNextImageLeft, 3000);
-//     setInterval(showNextImageRight, 3000);
-// });
-
-// ハンバーガーメニュー
-$(function(){
+  // ハンバーガーメニュー
   $("#p-drawer__toggle--sp").click(function(){
     $(this).toggleClass("open");
     $(".p-header__nav--sp").fadeToggle();
-    $(".c-hamburger_borderTop").toggleClass("open");
-    $(".c-hamburger_borderBottom").toggleClass("open");
+    $(".c-hamburger_borderTop, .c-hamburger_borderBottom").toggleClass("open");
   });
 
-  $("#p-drawer__toggle--sp").removeClass("open");
+  // 画像の切り替え
+  const imagesLeft = $('.p-header__left img');
+  const imagesRight = $('.p-header__right img');
+  let currentIndexLeft = 0;
+  let currentIndexRight = 0;
 
-  const mediaQuery = window.matchMedia('(max-width: 610px)');
+  function showNextImageLeft() {
+    const nextIndex = (currentIndexLeft + 1) % imagesLeft.length;
+    imagesLeft.eq(currentIndexLeft).animate({opacity: 0}, 500);
+    imagesLeft.eq(nextIndex).animate({opacity: 1}, 500);
+    currentIndexLeft = nextIndex;
+  }
 
-  const handleMediaQueryChange = (mediaQuery) => {
-    if (mediaQuery.matches) {
-      $(".p-header__nav--sp").hide();
-    }
-  };
+  function showNextImageRight() {
+    const nextIndex = (currentIndexRight + 1) % imagesRight.length;
+    imagesRight.eq(currentIndexRight).animate({opacity: 0}, 500);
+    imagesRight.eq(nextIndex).animate({opacity: 1}, 500);
+    currentIndexRight = nextIndex;
+  }
 
-  handleMediaQueryChange(mediaQuery);
-  mediaQuery.addListener(handleMediaQueryChange);
+  setInterval(showNextImageLeft, 3000);
+  setInterval(showNextImageRight, 3000);
 });
 
-
-
-
-  // const checkScrollSlideFadeLeft = (scrollBottom) => {
-  //   const scrollSlideFadeLeft = $("._scrollSlideFadeLeft");
-
-  //   scrollSlideFadeLeft.each((index, element) => {
-  //     const $element = $(element);
-  //     const elementTop = $element.offset().top;
-
-  //     if (elementTop < scrollBottom) {
-  //       $element.addClass("_isActiveSlideFadeLeft");
-  //     } else {
-  //       $element.removeClass("_isActiveSlideFadeLeft");
-  //     }
-  //   });
-  // };
-
-  // const checkScrollSlideFadeRight = (scrollBottom) => {
-  //   const scrollSlideFadeRight = $("._scrollSlideFadeRight");
-
-  //   scrollSlideFadeRight.each((index, element) => {
-  //     const $element = $(element);
-  //     const elementTop = $element.offset().top;
-
-  //     if (elementTop < scrollBottom) {
-  //       $element.addClass("_isActiveSlideFadeRight");
-  //     } else {
-  //       $element.removeClass("_isActiveSlideFadeRight");
-  //     }
-  //   });
-  // };
 
 
 

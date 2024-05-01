45b1e4a4e7dbd3b934db1edfbc9e8aaafe1cf251
{Marina <mari_ic5paniy@icloud.com>
2024-05-02 01:59:29 +0900

    top img

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
   // ページの読み込みが完了したらローディング画面をフェードアウト
   $(window).on("load", function() {
-    $("#loading-screen").fadeOut(1000);
+    $("#loading-screen").fadeOut(500);
     $("body").removeClass("loading"); // loadingクラスを削除してページを表示
   });
 
